6e58fe8e9b81e272ea451741b2f74319f00f2eac
nicoelias997 <nicoelias997@gmail.com>
2023-06-17 22:40:09 +0200

    Agrego los controladores para los empleados y para los departamentos donde aprendo a enviar info de tablas de BD y enviar a las vistas para poder usarlas ahi, mediante props

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,23 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Department;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class DepartmentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        $departments = Department::all();
+        return Inertia::render('Departments/Index', ['departments' => $departments]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
-        //
+        return Inertia::render('Departments/Create');
     }
 
     /**
@@ -28,38 +26,33 @@ class DepartmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:100'
+        ]);
+        $department = new Department($request->input());
+        $department->save();
+        return redirect('departments');
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Department $department)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(Department $department)
     {
-        //
+        return Inertia::render('Departments/Edit', ['deparment' => $department]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, Department $department)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:100'
+        ]);
+        $department->update($request->all());
+        return redirect('departments');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Department $department)
     {
-        //
+        $department->delete();
+        return redirect('departments');
     }
 }
--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,64 +2,95 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
 use App\Models\Employee;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB as FacadesDB;
+use Inertia\Inertia;
 class EmployeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
-    }
+        $employees = Employee::select('employees.id', 'employees.name', 'email', 'phone', 'department_id', 'departments.name as department')
+            ->join('departments')
+            ->paginate(10); //seleccionamos los empleados mediante sus id, para no confundir los departamentos, y los unimos a la tabla de departments
 
-    /**
-     * Show the form for creating a new resource.
-     */
+        $departments = Department::all(); //llamamos a toda la tabla de departments
+        return Inertia::render(
+            'Employees/Index',
+            [
+                'employees' => $employees,
+                'departments' => $departments
+            ]
+        ); //renderizo el componente a employes index, y envio como parametro los empleados y departamentos
+    }
     public function create()
     {
         //
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:150',
+            'email' => 'required|max:80',
+            'phone' => 'required|max:15',
+            'department_id' => 'required|numeric'
+        ]);
+
+        $employee = new Employee($request->input());
+        $employee->save();
+        return redirect('employees');
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Employee $employee)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Employee $employee)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Employee $employee)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:150',
+            'email' => 'required|max:80',
+            'phone' => 'required|max:15',
+            'department_id' => 'required|numeric'
+        ]);
+        $employee->update($request->input());
+        return redirect('employees');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Employee $employee)
     {
-        //
+        $employee->delete();
+        return redirect('employees');
     }
+    public function EmployeeByDepartment(){
+        $data = Employee::select(FacadesDB::raw('count(employee.id) as count, departments.name'))//selecciona de la base de dato y cuenta, lo renombramos como count, nombre de los departamentos
+            ->join('departments')
+            ->groupBy('departments.name')->get(); //lo unimos a la tabla de departments y lo organizamos por nombre de departamentos. 
+            return Inertia::render('Employees/Graphic', ['data' => $data]); //Inertia renderiza la data, hacia una vista, y enviamos la informacion mediante[props]
+    }
+    public function reports(){
+        $employees = Employee::select('employees.id', 'employees.name', 'email', 'phone', 'department_id', 'departments.name as department')
+            ->join('departments')
+            ->get(); //seleccionamos los empleados mediante sus id, para no confundir los departamentos, y los unimos a la tabla de departments y lo traemos
+
+        $departments = Department::all(); //llamamos a toda la tabla de departments
+        return Inertia::render(
+            'Employees/Reports',
+            [
+                'employees' => $employees,
+                'departments' => $departments
+            ]
+        ); //renderizo el componente a employes Reports, y envio como parametro los empleados y departamentos
+    }   
 }
+
+ 
